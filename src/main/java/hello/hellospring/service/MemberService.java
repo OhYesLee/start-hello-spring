72b1ea557d95f22c0f40047534494572ce8ddf78
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-11-11 15:25:34 +0900

    20221111

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-
+    ///// test 파일 단축키 cmd + shift + T///////
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     /**
